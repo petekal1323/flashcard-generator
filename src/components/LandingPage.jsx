eaca28c21c73a512d239f2e56fea2ecf0917b7f6
Peter Kalinowski <petekal1323@gmail.com>
2025-03-09 03:11:21 -0400

    Finalize LandingPage component with styles

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,9 +5,10 @@ function LandingPage(props) {
         <div className='landing__container'>
             <header className='landing__header'>
                 <h1 className='landing__title'>AI-Powered Flashcard Generator</h1>
-                <p className='landing__subtitle'>Create flashcards with the help of AI. Just type in the text you want to learn and the AI will generate flashcards for you.</p>
-                <p className='landing__subtitle'>Or you can upload a text file and the AI will generate flashcards for you.</p>
-                <button className='landing__button' onClick={props.onStart}>Get Started</button>
+                <p className='landing__subtitle'>
+                Enter your text or upload a file, and the AI will create flashcards for you.
+                </p>
+                <button className='landing__cta' onClick={props.onStart}>Get Started</button>
             </header>
         </div>
     );
