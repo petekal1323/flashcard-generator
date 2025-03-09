eef75e02b320fc2a2843a2bc27107b191ea5c64a
Peter Kalinowski <petekal1323@gmail.com>
2025-03-09 03:16:09 -0400

    update dependencies and also vite config for network access for phone testing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server: {
+  //   host: true, // Allows your dev server to be accessible externally (e.g., from your phone)
+  // },
 })
